b0a49815b7646be8a34e3ce0354acf377ddb02e4
Mmdva080 <145641292+Mmdva080@users.noreply.github.com>
2025-02-05 01:40:18 -0800

    login.php

--- login.php
+++ login.php
@@ -6,12 +6,15 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $query = "SELECT * FROM members WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($conn, $query);
+    // استفاده از پرس‌وجوهای آماده برای امنیت بیشتر
+    $query = $conn->prepare("SELECT * FROM members WHERE username = ? AND password = ?");
+    $query->bind_param('ss', $username, $password);
+    $query->execute();
+    $result = $query->get_result();
 
-    if (mysqli_num_rows($result) == 1) {
+    if ($result->num_rows == 1) {
         $_SESSION['username'] = $username;
-        header('Location: index.html');
+        header('Location: index.php'); // تغییر به index.php
         exit();
     } else {
         echo "نام کاربری یا رمز عبور اشتباه است.";
