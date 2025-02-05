1af26e8970096c6a1ea8217f5f770c0be69b7f10
Mmdva080 <145641292+Mmdva080@users.noreply.github.com>
2025-02-05 01:39:52 -0800

    signup.php

--- signup.php
+++ signup.php
@@ -7,10 +7,14 @@ if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['passwor
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $query = "INSERT INTO members (username, email, password) VALUES ('$username', '$email', '$password')";
-    if (mysqli_query($conn, $query)) {
+    // اضافه کردن کاربر جدید به دیتابیس
+    $query = $conn->prepare("INSERT INTO members (username, email, password) VALUES (?, ?, ?)");
+    $query->bind_param('sss', $username, $email, $password);
+    $query->execute();
+
+    if ($query->affected_rows == 1) {
         $_SESSION['username'] = $username;
-        header('Location: index.html');
+        header('Location: index.php'); // تغییر به index.php
         exit();
     } else {
         echo "خطا در ثبت‌نام. لطفا دوباره تلاش کنید.";
